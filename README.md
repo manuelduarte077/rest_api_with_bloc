e57be46dc4f3d6168429238dbb26db683a2f6e99
@manuelduarte077 <carlosd99m@gmail.com>
2022-04-21 16:17:16 -0600

    project setup

---
+++ README.md
@@ -0,0 +1,12 @@
+# flutter_api_with_bloc
+
+A Flutter developer assignment.
+
+## Getting Started
+
+This is a Flutter developer assignment solution,
+I decided to use Block Stat Management as a challenge for myself by learning something new for me and I used several flutter_bloc/webview_flutter/equatable libraries .
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_api_with_bloc/repositories/universities_repository.dart';
+import 'package:flutter_api_with_bloc/screens/home_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc_state/universities_block.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    //To Communicate UI with Service layer
+    return MaterialApp(
+      home: BlocProvider(
+          create: (context) => UniversitiesBloc(
+                universitiesRepository: UniversitiesRepository(),
+              ),
+          child: HomeScreen()),
+    );
+  }
+}
